a1580cd2b49c8e25cf33627d2b2f1cd4dbfc319c
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-01-31 14:10:28 +0530

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,7 @@
+HELP_TEXT = """👊Cᴏɴᴛᴀᴄᴛ ʀᴇᴘᴏʀᴛ ʙᴜɢ :-@Anjel_Neha"""
+
+ABOUT_TEXT = """
+<b>⚜ Mʏ ɴᴀᴍᴇ :- ShareusBypasserBot</b>\n
+<b>🔸Vᴇʀꜱɪᴏɴ :- <a href='https://telegram.me/VJ_Bots'>1.0</a></b>\n
+<b>🔹Dᴇᴠᴇʟᴏᴘᴇʀ :- <a href='https://t.me/Anjel_Neha'>Anjel Neha</a></b>\n
+<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://telegram.me/VJ_Bots'>[ 19-12-2022 ]</a></b>"""
